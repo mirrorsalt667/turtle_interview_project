560932e376b5bf06f85e20bb8e7e20f960779009
StephenMBAM1 <jjjjllears@icloud.com>
2023-09-10 09:53:14 +0800

    Initial Commit

---
+++ turtle_interview_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  turtle_interview_project
+//
+//  Created by Stephen on 2023/9/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
